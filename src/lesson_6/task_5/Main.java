b63d50e7b2d816457915cec6e5f690db04c8e6d9
ZAS <777ergoproxy@gmail.com>
2024-09-17 11:20:42 +0300

    fix task_5

--- src/lesson_6/task_5/Main.java
+++ src/lesson_6/task_5/Main.java
@@ -1,5 +1,6 @@
 package lesson_6.task_5;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -12,18 +13,28 @@ public class Main {
         var console = new Scanner(System.in);
         var stringArray = new String[6];
 
+
         System.out.println("Enter 6 lines");
         for (int i = 0; i < 6; i++) {
             stringArray[i] = console.nextLine();
         }
-
+        var temp = Arrays.copyOf(stringArray, stringArray.length);
         for (int i = 0; i < stringArray.length; i++) {
+            if (stringArray[i] == null) {
+                continue;
+            }
             for (int j = i + 1; j < stringArray.length; j++) {
-                if (stringArray[i] != null && stringArray[i].equals(stringArray[j])) {
+                if (stringArray[i].equals(stringArray[j])) {
                     stringArray[j] = null;
+                    temp[i] = null;
                 }
             }
+            if (temp[i] == null) {
+                stringArray[i] = null;
+            }
+
         }
+
         for (String s : stringArray) {
             System.out.println(s);
         }
