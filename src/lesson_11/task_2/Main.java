07fcd3f0cda126461bf47f20a336420bb4504379
ZAS <777ergoproxy@gmail.com>
2024-10-03 12:29:39 +0300

    lesson_12 upgrade

--- src/lesson_11/task_2/Main.java
+++ src/lesson_11/task_2/Main.java
@@ -1,7 +1,6 @@
 package lesson_11.task_2;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,13 +9,10 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<String> arrayList = new ArrayList<>();
-        LinkedList<String> linkedList = new LinkedList<>();
         Scanner console = new Scanner(System.in);
 
         System.out.println("Array List");
         fillLines(arrayList, console);
-        System.out.println("Linked List");
-        fillLines(linkedList, console);
 
         console.close();
     }
@@ -26,15 +22,12 @@ public class Main {
         while (strings.size() < 5) {
             strings.addFirst(console.nextLine());
         }
-        long startTime = System.nanoTime();
         for (int i = 0; i < 13; i++) {
             String temp = strings.getLast();
             strings.removeLast();
             strings.addFirst(temp);
         }
-        long finishTime = System.nanoTime();
-        long elapsed = (finishTime - startTime) / 1000;
-        System.out.println("operating time: " + elapsed + " milliseconds");
+
 
         for (String string : strings) {
             System.out.println(string);
--- src/lesson_11/task_3/Main.java
+++ src/lesson_11/task_3/Main.java
@@ -1,7 +1,6 @@
 package lesson_11.task_3;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,13 +9,10 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<String> arrayList = new ArrayList<>();
-        LinkedList<String> linkedList = new LinkedList<>();
         Scanner console = new Scanner(System.in);
 
         System.out.println("Array List");
         fillLines(arrayList, console);
-        System.out.println("Linked List");
-        fillLines(linkedList, console);
 
         console.close();
     }
@@ -26,7 +22,6 @@ public class Main {
         while (strings.size() < 10) {
             strings.add(console.nextLine());
         }
-        long startTime = System.nanoTime();
         String shortest = strings.getFirst();
         String longest = strings.getFirst();
 
@@ -47,9 +42,6 @@ public class Main {
         } else {
             System.out.println("First longest line: " + longest);
         }
-        long finishTime = System.nanoTime();
-        long elapsed = (finishTime - startTime) / 1000;
-        System.out.println("operating time: " + elapsed + " milliseconds");
 
     }
 }
